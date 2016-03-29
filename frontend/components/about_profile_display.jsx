413cfb750c2c495a4990b1d099fa0b6aa23dd151
Dan Phillips <danmakenoise@gmail.com>
2016-03-29 18:52:14 -0400

    complete basic profile styling

--- frontend/components/about_profile_display.jsx
+++ frontend/components/about_profile_display.jsx
@@ -22,14 +22,32 @@ var AboutProfileDisplay = React.createClass({
       var fullName = profile.first_name + ' ' + profile.last_name;
       var gender = profile.gender === 'M' ? 'Male' : 'Female';
       var birthday = profile.birthday;
+      var location = profile.location ? profile.location : '';
 
       return (
         <div className='about-profile-display'>
-          <h1>{ fullName }</h1>
-          <ul>
-            <li>{ gender }</li>
-            <li>Born on { birthday }</li>
-          </ul>
+          <header>
+            <h1>About { fullName }</h1>
+          </header>
+          <div className='about-profile-information'>
+            <div className='about-section-header'>
+              <span>Basic Information</span>
+            </div>
+            <div className='about-section-contents'>
+              <div className='about-section-attribute'>
+                <span className='about-tag'>Gender</span>
+                <span className='about-data'>{ gender }</span>
+              </div>
+              <div className='about-section-attribute'>
+                <span className='about-tag'>Birthday</span>
+                <span className='about-data'>{ birthday }</span>
+              </div>
+              <div className='about-section-attribute'>
+                <span className='about-tag'>Location</span>
+                <span className='about-data'>{ location }</span>
+              </div>
+            </div>
+          </div>
         </div>
       );
     } else {
