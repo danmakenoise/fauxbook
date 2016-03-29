d6368c5dceb6a4b7d955147414d81862d0c8dc0e
Dan Phillips <danmakenoise@gmail.com>
2016-03-29 11:10:42 -0400

    add database seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ActiveRecord::Base
 
   after_initialize :ensure_session_token
 
+  attr_reader :password
+  
   def password= password
     self.password_digest = BCrypt::Password.create password
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create! email: 'fauxbook@fauxbook.com', password: 'password'
+
+10.times do
+  User.create! email: Faker::Internet.email, password: 'password'
+end
