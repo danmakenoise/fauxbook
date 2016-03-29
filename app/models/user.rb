a2bdc3abaf1e4fc353b7bb79f7db2e707931fd2e
Dan Phillips <danmakenoise@gmail.com>
2016-03-29 11:18:27 -0400

    add password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,17 @@ class User < ActiveRecord::Base
   after_initialize :ensure_session_token
 
   attr_reader :password
-  
+
+  def User.find_by_credentials email, password
+    user = User.find_by email: email
+    return nil unless user
+    return user if user.is_password? password
+  end
+
+  def is_password? password
+    BCrypt::Password.new( self.password_digest ).is_password? password
+  end
+
   def password= password
     self.password_digest = BCrypt::Password.create password
   end
