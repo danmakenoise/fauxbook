87f8971a24cf9adf4af3b9c7e78699e06128f8cc
Dan Phillips <danmakenoise@gmail.com>
2016-03-30 12:30:06 -0400

    enable default profile picture

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,5 @@
 class Profile < ActiveRecord::Base
-  has_attached_file :profile_picture, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: ActionController::Base.helpers.image_path("/default_profile_picture.jpg")
+  has_attached_file :profile_picture, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: ActionController::Base.helpers.image_path("default_profile_picture.jpg")
 
   validates :user_id, :first_name, :last_name, :birthday, :gender, presence: true
   validates :gender, inclusion: { in: %w(M F), message: 'Please pick a gender' }
--- frontend/components/profile_picture.jsx
+++ frontend/components/profile_picture.jsx
@@ -1,9 +1,16 @@
 var React = require( 'react' );
 
 var ProfilePicture = React.createClass({
-
   render: function () {
-    return <div className='profile-picture'>Profile Picture</div>;
+    if ( this.props.profile ) {
+      return (
+        <div className='profile-picture'>
+          <img src={ this.props.profile.profile_picture } />
+        </div>
+      );
+    } else {
+      return <div className='profile-picture'></div>;
+    }
   }
 });
 
