316fc8248ce1b2d5271e1f046883bfbe8962af1c
Dan Phillips <danmakenoise@gmail.com>
2016-03-31 11:10:49 -0400

    remove back-end redirection if not logged in

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,13 +1,5 @@
 class StaticPagesController < ApplicationController
-  before_action :ensure_logged_in
-
   def root
     render :root
-  end
-
-  private
-
-  def ensure_logged_in
-    redirect_to login_url unless logged_in?
   end
 end
