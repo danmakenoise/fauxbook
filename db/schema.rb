c881517238d346b0c30a18f53d2863481536c317
Dan Phillips <danmakenoise@gmail.com>
2016-04-01 10:49:05 -0400

    create photo column for posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160331133716) do
+ActiveRecord::Schema.define(version: 20160401144842) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
-    t.integer  "author_id",  null: false
-    t.integer  "profile_id", null: false
-    t.text     "body",       null: false
+    t.integer  "author_id",          null: false
+    t.integer  "profile_id",         null: false
+    t.text     "body",               null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
   add_index "posts", ["author_id"], name: "index_posts_on_author_id", using: :btree
