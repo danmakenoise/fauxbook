32a6f61fd963b483baa0f774909f55e0b7cf9849
Dan Phillips <danmakenoise@gmail.com>
2016-04-05 11:01:52 -0400

    create comments migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160403203207) do
+ActiveRecord::Schema.define(version: 20160405145707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "body",             null: false
+    t.integer  "commentable_id",   null: false
+    t.string   "commentable_type", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["commentable_id"], name: "index_comments_on_commentable_id", using: :btree
 
   create_table "friendships", force: :cascade do |t|
     t.integer  "user_id",                    null: false
