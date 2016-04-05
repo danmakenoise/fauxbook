4227597c8fb04e678c7ce3b911882e13f8b4c287
Dan Phillips <danmakenoise@gmail.com>
2016-04-05 11:13:46 -0400

    add author to comments model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,7 @@ class Comment < ActiveRecord::Base
   validates :body, :commentable_id, :commentable_type, presence: true
 
   belongs_to :commentable, polymorphic: true
+  belongs_to :author, class_name: 'User'
 
   has_many :comments, as: :commentable
 end
