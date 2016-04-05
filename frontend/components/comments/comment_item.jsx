f979a92d16def899eca153d3f05c5f4f9534bd99
Dan Phillips <danmakenoise@gmail.com>
2016-04-05 15:38:28 -0400

    style comment show

--- frontend/components/comments/comment_item.jsx
+++ frontend/components/comments/comment_item.jsx
@@ -1,7 +1,12 @@
 var React = require( 'react' );
 var ProfilePicture = require( '../profile_picture' );
+var SessionStore = require( '../../stores/session_store' );
 
 var CommentItem = React.createClass({
+  getInitialState: function () {
+    return { deleting: false };
+  },
+
   render: function () {
     return (
       <div className='comment-container'>
@@ -10,13 +15,37 @@ var CommentItem = React.createClass({
           image={ this.props.comment.author.image }
         />
       <a
+        className='comment-author'
         href={ '/#/users/' + this.props.comment.author_id }
       >
         { this.props.comment.author.name }
       </a>
-      <span>{ this.props.comment.body }</span>
+      <span className='comment-body'>{ this.props.comment.body }</span>
+      { this._commentDeleteButton() }
       </div>
     );
+  },
+
+  _commentDeleteButton: function () {
+    if ( SessionStore.currentUserId() === this.props.comment.author_id ) {
+      var buttonText = this.state.deleting ? 'Are You Sure?' : 'Delete';
+      var activeClass = this.state.deleting ? '' : ' transparent';
+      var buttonClass = 'comment-delete-button' + activeClass;
+
+      return (
+        <button className={ buttonClass } onClick={ this._deleteComment }>
+          { buttonText }
+        </button>
+      );
+    }
+  },
+
+  _deleteComment: function () {
+    if ( this.state.deleting ) {
+      // delete comment
+    } else {
+      this.setState( { deleting: true } );
+    }
   }
 });
 
