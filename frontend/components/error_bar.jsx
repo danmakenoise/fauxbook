79af217e24a990bc33c7955458478692280d5e28
Dan Phillips <danmakenoise@gmail.com>
2016-04-07 15:05:42 -0400

    implement random quotes for error reporting

--- frontend/components/error_bar.jsx
+++ frontend/components/error_bar.jsx
@@ -26,9 +26,48 @@ var ErrorBar = React.createClass({
     }
   },
 
+  _errorMessages: function () {
+    return [
+      { author: 'Rene Descartes',
+        quote: 'I am indeed amazed when I consider how weak my mind is and ' +
+          'how prone to error.' },
+      { author: 'Voltaire',
+        quote: 'The progress of rivers to the ocean is not so rapid as that ' +
+          'of man to error.' },
+      { author: 'Orlando Aloysius Battista',
+        quote: 'An error doesn\'t become a mistake until you refuse to ' +
+          "correct it." },
+      { author: 'William Hudson',
+        quote: 'Game over, man. Game over!' },
+      { author: 'Winston Churchill',
+        quote: 'All men make mistakes, but only wise men learn from their ' +
+          'mistakes.' },
+      { author: 'Albert Einstein',
+        quote: 'A person who never made a mistake never tried anything new.' }
+    ];
+  },
+
+  _randomErrorMessage: function () {
+    messages = this._errorMessages();
+    randomIndex = Math.floor( Math.random() * messages.length );
+    quote = messages[randomIndex];
+
+    return (
+      <div className='error-bar__error-quote'>
+        <h1 className='error-quote__quote'>
+          { '"' + quote.quote + '"' }
+        </h1>
+        <p className='error-quote__author'>
+          { '-' + quote.author }
+        </p>
+      </div>
+    );
+  },
+
   _renderErrorList: function () {
     return (
       <div className='error-bar bar--shadowed'>
+        { this._randomErrorMessage() }
         <ul className='error-bar__error-list'>
           { this._renderErrorItems() }
         </ul>
