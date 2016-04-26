557f26b7b01b2b4eb7c0382758cf3af4bade0773
Dan Phillips <danmakenoise@gmail.com>
2016-04-26 16:10:27 -0400

    pull out sign in method

---
+++ spec/support/sign_in_helper.rb
@@ -0,0 +1,11 @@
+def sign_in(user = nil)
+  user ||= FactoryGirl.create(:user)
+  visit '/'
+  within '.sign-in' do
+    fill_in 'user[email]', with: user.email
+    fill_in 'user[password]', with: 'password'
+  end
+  click_on 'Log In'
+
+  user
+end
