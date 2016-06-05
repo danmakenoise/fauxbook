49909ad5252e617f346ede733fe7bd211b495105
Dan Phillips <danmakenoise@gmail.com>
2016-06-04 23:56:33 -0400

    fix orphaned likes issue

--- app/models/concerns/likeable.rb
+++ app/models/concerns/likeable.rb
@@ -2,7 +2,7 @@ module Likeable
   extend ActiveSupport::Concern
 
   included do
-    has_many :likes, as: :likeable
+    has_many :likes, as: :likeable, dependent: :destroy
     has_many :likers, through: :likes, source: :user
   end
 end
